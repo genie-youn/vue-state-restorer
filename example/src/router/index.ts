b35ee1099ec23efb1c6fcad7240d1c853f65132d
jisoo-youn <yjs930915@gmail.com>
2019-11-13 18:21:20 +0900

    add travels routing

--- example/src/router/index.ts
+++ example/src/router/index.ts
@@ -18,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/travels/my',
+    name: 'travels',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Travels.vue'),
+  },
 ];
 
 const router = new VueRouter({
